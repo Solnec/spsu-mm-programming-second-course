68e150f02c8f4ca5055ca9f554242a0e81d1295d
Leonova Anna <aniliar@mail.ru>
2015-11-04 22:55:23 +0400

    add Filters

---
+++ Autumn/Filters/Filters/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Filters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть изображение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(184, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(560, 35);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(732, 381);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(750, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(161, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Размытие по Гауссу";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(750, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Фильтр Собеля";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 446);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Обработка изображений";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Autumn/Filters/Filters/Form1.cs
@@ -0,0 +1,166 @@
+﻿//Leonova Anna
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Filters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = new Bitmap(openFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bm = (Bitmap)pictureBox1.Image;
+                Blur(bm);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Bitmap bm = (Bitmap)pictureBox1.Image;
+                FilterSobel(bm);
+            }
+        }
+
+        private void FilterSobel(Bitmap bm)
+        {            
+            int w = bm.Width;
+            int h = bm.Height;
+            double ind = 0;
+            int[,] gx = new int[,] { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+            int[,] gy = new int[,] { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+
+            int[,] allPixR = new int[w, h];
+            int[,] allPixG = new int[w, h];
+            int[,] allPixB = new int[w, h];
+
+            int limit = 128 * 128;
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    allPixR[i, j] = bm.GetPixel(i, j).R;
+                    allPixG[i, j] = bm.GetPixel(i, j).G;
+                    allPixB[i, j] = bm.GetPixel(i, j).B;
+                }
+            }
+
+            int new_rx, new_ry;
+            int new_gx, new_gy;
+            int new_bx, new_by;
+            int rc, gc, bc;
+            for (int i = 1; i < bm.Width - 1; i++)
+            {
+                for (int j = 1; j < bm.Height - 1; j++)
+                {
+                    new_rx = 0;
+                    new_ry = 0;
+                    new_gx = 0;
+                    new_gy = 0;
+                    new_bx = 0;
+                    new_by = 0;
+                    rc = 0;
+                    gc = 0;
+                    bc = 0;
+
+                    for (int wi = -1; wi < 2; wi++)
+                    {
+                        for (int hw = -1; hw < 2; hw++)
+                        {
+                            ind = (i * 1.0 / (bm.Width - 2));
+                            progressBar1.Value = (int)(ind * 100);
+                            Application.DoEvents();
+
+                            rc = allPixR[i + hw, j + wi];
+                            new_rx += gx[wi + 1, hw + 1] * rc;
+                            new_ry += gy[wi + 1, hw + 1] * rc;
+
+                            gc = allPixG[i + hw, j + wi];
+                            new_gx += gx[wi + 1, hw + 1] * gc;
+                            new_gy += gy[wi + 1, hw + 1] * gc;
+
+                            bc = allPixB[i + hw, j + wi];
+                            new_bx += gx[wi + 1, hw + 1] * bc;
+                            new_by += gy[wi + 1, hw + 1] * bc;
+                        }
+                    }
+                    if (new_rx * new_rx + new_ry * new_ry > limit || new_gx * new_gx + new_gy * new_gy > limit || new_bx * new_bx + new_by * new_by > limit)
+                        bm.SetPixel(i, j, Color.Transparent);
+
+                    else
+                        bm.SetPixel(i, j, Color.Black);
+                }
+            }
+            progressBar1.Value = 0;
+            pictureBox1.Refresh();
+        }
+
+        private void Blur(Bitmap bm)
+        {
+            int w = bm.Width;
+            int h = bm.Height;
+            double ind = 0;
+
+            // horizontal blur
+            for (int i = 1; i < w - 1; i++)
+            {
+                ind = (i * 0.5 / w);
+                progressBar1.Value = (int)(ind * 100);
+                for (int j = 0; j < h; j++)
+                {
+                    Application.DoEvents();
+                    Color c1 = bm.GetPixel(i - 1, j);
+                    Color c2 = bm.GetPixel(i, j);
+                    Color c3 = bm.GetPixel(i + 1, j);
+
+                    byte gR = (byte)((c1.R + c2.R + c3.R) / 3);
+                    byte gG = (byte)((c1.G + c2.G + c3.G) / 3);
+                    byte gB = (byte)((c1.B + c2.B + c3.B) / 3);
+
+                    Color cBlured = Color.FromArgb(gR, gG, gB);
+                    bm.SetPixel(i, j, cBlured);
+                }
+            }
+
+            // vertical blur
+            for (int i = 0; i < w; i++)
+            {
+                ind = 0.5 + i * 0.5 / (w - 1);
+                progressBar1.Value = (int)(ind * 100);
+                for (int j = 1; j < h - 1; j++)
+                {
+                    Application.DoEvents();
+                    Color c1 = bm.GetPixel(i, j - 1);
+                    Color c2 = bm.GetPixel(i, j);
+                    Color c3 = bm.GetPixel(i, j + 1);
+
+                    byte gR = (byte)((c1.R + c2.R + c3.R) / 3);
+                    byte gG = (byte)((c1.G + c2.G + c3.G) / 3);
+                    byte gB = (byte)((c1.B + c2.B + c3.B) / 3);
+
+                    Color cBlured = Color.FromArgb(gR, gG, gB);
+                    bm.SetPixel(i, j, cBlured);
+                }
+            }
+            progressBar1.Value = 0;
+            pictureBox1.Refresh();
+        }
+    }
+}
