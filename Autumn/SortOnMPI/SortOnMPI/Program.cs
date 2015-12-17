b0977d116d08df46d8198620f5c0924c697d11d6
Alexander Misonizhnik <misonijnik@gmail.com>
2015-12-17 15:56:05 +0300

    - update MPI.

--- Autumn/SortOnMPI/SortOnMPI/Program.cs
+++ Autumn/SortOnMPI/SortOnMPI/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using MPI;
 using Sort;
 
 namespace SortOnMPI
@@ -11,12 +13,19 @@ namespace SortOnMPI
     {
         private static void Main(string[] args)
         {
-            int[] arr = { 100, 86, 54, 75, 12, 64, 32, 19, 48, 76, 51, 87, 16, 24, 81, 42, 36, 99, 1, -1 };
-            //int[] arr = { 4, 3, 2, 1 };
-            Sort.Sort.SortArray(arr, args);
-            foreach (var i in arr)
+            using (new MPI.Environment(ref args))
             {
-                Console.WriteLine(i);
+
+                int[] arr = { 100, 86, 54, 75, 12, 64, 32, 19, 48, 76, 51, 87, 16, 24, 81, 42, 36, 99, 1, -1, 1000000, 0, 0, 1000000, 703, 2 };
+                //int[] arr = { 4, 3, 2, 1 };
+                Sort.Sort.SortArray(arr, args);
+                if (Communicator.world.Rank == 0)
+                {
+                    foreach (var i in arr)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
             }
         }
     }
