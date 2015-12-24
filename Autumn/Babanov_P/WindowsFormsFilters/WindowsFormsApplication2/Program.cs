c45fde4305badec78607e3fcdb96e9f248b6d7fa
PetrB09 <PetrB07@yandex.ru>
2015-12-24 13:57:06 +0300

    - changed lab "WinFormsFilters"

--- Autumn/Babanov_P/WindowsFormsFilters/WindowsFormsApplication2/Program.cs
+++ Autumn/Babanov_P/WindowsFormsFilters/WindowsFormsApplication2/Program.cs
@@ -16,7 +16,7 @@ namespace WinFormsFilters
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
